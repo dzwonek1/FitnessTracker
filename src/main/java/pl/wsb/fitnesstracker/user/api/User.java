e31b5aa42687821ed7e7249899c1aaf616080e81
grzegorzkaczmarek002 <grzegorz.kaczmarek002@gmail.com>
2026-04-23 22:25:04 +0200

    Poprawa

--- src/main/java/pl/wsb/fitnesstracker/user/api/User.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/User.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
